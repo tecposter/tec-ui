c8e06c53a1a1f36c35369c2ed2f185ff8530505e
zjh <zhanjh@126.com>
2018-09-29 16:25:38 +0800

    init

---
+++ script/frontServer.js
@@ -0,0 +1,39 @@
+#!/usr/bin/env node
+
+/* eslint-env node */
+/* eslint-disable no-console */
+
+'use strict';
+
+const path = require('path');
+const localSetting = require('./setting.local');
+
+const baseDir = path.resolve(__dirname, '..');
+const port = localSetting.front.port || 8007;
+const staticHost = localSetting.site.static.host || 'localhost';
+
+const front = require('gap-node-front')({
+    baseDir: baseDir,
+    port: port,
+    staticHost: staticHost,
+    scss: {
+        publicSlug: {
+            dev: 'ui/dev/css',
+            dist: 'ui/dist/css'
+        },
+        inputDir: 'src/scss',
+        outputDir: {
+            dev: 'site/static/ui/dev/css',
+            dist: 'site/static/ui/dist/css'
+        },
+        includePaths: []
+    }
+});
+
+const cmd = process.argv[2];
+
+if (cmd === 'server') {
+    front.runServer();
+} else if (cmd === 'release') {
+    front.release();
+}
---
+++ script/setting.local.js
@@ -0,0 +1,12 @@
+/* eslint-env node */
+
+module.exports = {
+    front: {
+        port: 8789	
+    },
+    site: {
+        static: {
+            host: 'static.tecposter.cn'
+        }
+    }
+};
